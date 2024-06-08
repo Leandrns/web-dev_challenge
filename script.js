f800c31dc9123edabc0763398d50b1bfb2eef423
Leandro Souza <leandro2005souza@gmail.com>
2024-06-08 17:22:10 -0300

    Concluindo menu de navegação

---
+++ script.js
@@ -0,0 +1,11 @@
+// MENU DROPDOWN
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const menuDropDown = document.querySelector('.menu_dropdown');
+
+toggleBtn.onclick = function () {
+    menuDropDown.classList.toggle('open')
+
+    const isOpen = menuDropDown.classList.contains('open')
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+}
