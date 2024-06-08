7eb908c7f8713e932840038ea6ad7a7f8ea5d86c
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-06-08 20:43:46 -0300

    slider

--- script.js
+++ script.js
@@ -4,14 +4,31 @@ const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const menuDropDown = document.querySelector('.menu_dropdown');
 
 toggleBtn.onclick = function () {
-    menuDropDown.classList.toggle('open')
+    menuDropDown.classList.toggle('open');
 
-    const isOpen = menuDropDown.classList.contains('open')
-    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+    const isOpen = menuDropDown.classList.contains('open');
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 }
 
-const chk = document.getElementById('chk')
+const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
-    document.body.classList.toggle('dark')
+    document.body.classList.toggle('dark');
 })
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval (function(){
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>3){
+        count = 1
+    }
+    
+    document.getElementById("radio"+count).checked = true;
+}
+
